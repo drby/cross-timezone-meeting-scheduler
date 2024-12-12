f329159be991feae9506a67d47a36eed3b3b6644
tom darby <51497148+drby@users.noreply.github.com>
2024-12-12 14:32:10 -0500

    Initialise React calendar in calendar page + fixed tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,17 @@
 /** @type {import('tailwindcss').Config} */
-const config = {
-  content: [],
+module.exports = {
+  content: [
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        background: "#131c2f",
+        foreground: "#eee",
+      },
+    },
   },
   plugins: [],
-};
-
-export default config;
-
+}
